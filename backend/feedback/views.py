540289f6238903644d2d02726305b03d2d3f48a4
varun <varunteja007006@gmail.com>
2024-09-30 22:40:15 +0530

    update the post api feedback

--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -7,9 +7,17 @@ from feedback.serializers import FeedbackSerializer
 # Create your views here.
 @csrf_exempt
 def feedback(request):
+    # Post request handled here
     if request.method == "POST":
-        return
-    if request.method == "GET":
+        data = JSONParser().parse(request)
+        serializer = FeedbackSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    # get request handled here
+    elif request.method == "GET":
         feedback = models.Feedback.objects.all()
         serializer = FeedbackSerializer(feedback, many=True)
         return JsonResponse(serializer.data, safe=False)
